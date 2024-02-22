8cfe37a1045c9d5b932e49918a349c71c2609f3f
kevin-ngo1 <kevin.ngo@laplateforme.io>
2024-02-22 15:24:18 +0100

    Put rect for button messages, channels

--- homepage/BasepageInterface.py
+++ homepage/BasepageInterface.py
@@ -1,4 +1,6 @@
 import pygame
+from Bouton import Button
+
 
 class BasePage():
     def __init__(self, screen):
@@ -18,6 +20,11 @@ class BasePage():
     def update(self):
         self.screen.blit(self.background, (0, 0))
         pygame.draw.rect(self.screen, self.grey, (50, 50,300,self.screen_height - 90), border_radius=30)
+        pygame.draw.rect(self.screen, self.white, (70, 220,260,70), border_radius=30)
+        pygame.draw.rect(self.screen, self.white, (70, 360,260,70), border_radius=30)
+        pygame.draw.rect(self.screen, self.white, (70, 4600,260,70), border_radius=30)
         pygame.draw.circle(self.screen, self.white, (110, 120), 40)
+        connexion_button = Button("Connexion", (300, 560), (36, 15), None, self.white, width=200, height=100)
+        connexion_button.draw(self.screen)
         pygame.display.update()
 
