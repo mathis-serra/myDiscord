1bb19666ee230dc25a0d50e9d68759dcbde5eb8b
kevin-ngo1 <kevin.ngo@laplateforme.io>
2024-02-22 20:51:59 +0100

    Button working for changing Message, Profil

--- homepage/BasepageInterface.py
+++ homepage/BasepageInterface.py
@@ -8,6 +8,9 @@ class BasePage():
         self.screen_width, self.screen_height = screen.get_size()
         self.grey="#5c5959"
         self.white="#ffffff"
+        self.blue = "#1769aa"
+        self.beige="#d0c8b6"
+        self.font_before = pygame.font.Font("Data/Font/MickeyMouse.otf", 45)
         self.background = pygame.image.load('Data/Pictures/home_background.png')
         self.background = pygame.transform.scale(self.background, (self.screen_width, self.screen_height))
         self.run = True
@@ -17,14 +20,23 @@ class BasePage():
             if event.type == pygame.QUIT:
                 self.run = False
 
+    def test(self):
+        print("oui")
+
     def update(self):
         self.screen.blit(self.background, (0, 0))
-        pygame.draw.rect(self.screen, self.grey, (50, 50,300,self.screen_height - 90), border_radius=30)
-        pygame.draw.rect(self.screen, self.white, (70, 220,260,70), border_radius=30)
-        pygame.draw.rect(self.screen, self.white, (70, 360,260,70), border_radius=30)
-        pygame.draw.rect(self.screen, self.white, (70, 4600,260,70), border_radius=30)
+
+        pygame.draw.rect(self.screen, self.beige, (50, 50,300,self.screen_height - 90), border_radius=30)
+        pygame.draw.rect(self.screen, self.white, (70, 220,260,65), border_radius=30)
+        pygame.draw.rect(self.screen, self.white, (70, 340,260,65), border_radius=30)
+        pygame.draw.rect(self.screen, self.white, (70, 460,260,65), border_radius=30)
         pygame.draw.circle(self.screen, self.white, (110, 120), 40)
-        connexion_button = Button("Connexion", (300, 560), (36, 15), None, self.white, width=200, height=100)
-        connexion_button.draw(self.screen)
-        pygame.display.update()
+
+        messages_text_render = self.font_before.render("Messages", True, self.blue)
+        messages_text_rect = messages_text_render.get_rect(topleft=(125, 235))
+        self.screen.blit(messages_text_render, messages_text_rect)
+
+        channels_text_render = self.font_before.render("Channels", True, self.blue)
+        channels_text_rect = channels_text_render.get_rect(topleft=(128, 355))
+        self.screen.blit(channels_text_render, channels_text_rect)
 
--- homepage/MessagesInterface.py
+++ homepage/MessagesInterface.py
@@ -1,11 +1,10 @@
 import pygame
 from BasepageInterface import BasePage
 
-class Profil(BasePage):
+class Messages(BasePage):
     def __init__(self,screen):
         super().__init__(screen)
 
     def new_rect(self):
-        pygame.draw.rect(self.screen, self.beige, (500, 100,500,self.screen_height - 200), border_radius=30)
+        pygame.draw.rect(self.screen, self.beige, (500, 50,500,self.screen_height - 90), border_radius=30)
         pygame.display.update()
-
