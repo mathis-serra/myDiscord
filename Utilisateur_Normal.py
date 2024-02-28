ba6003ca9918c7f63c414f58daeb10d99f76f593
mathis-serra <mathis.serra@laplateforme.io>
2024-02-28 13:55:56 +0100

    delete wrong files

--- Utilisateur_Normal.py
+++
@@ -1,5 +0,0 @@
-from Utilisateur import User
-
-class NormalUser(User):
-    def __init__(self, id, nom, prenom,password, email,role):
-        super().__init__(id, nom, prenom,password, email,role)
