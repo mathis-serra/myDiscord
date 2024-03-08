4acf20eca8f0525f9f758f83cf45546679c8944f
mathis-serra <mathis.serra@laplateforme.io>
2024-03-08 15:08:40 +0100

    merge server into main last merge

--- server/settings.py
+++ server/settings.py
@@ -5,7 +5,7 @@ class settings():
     db = mysql.connect(
         host="localhost",
         user="root",
-        passwd="n21217916",
+        passwd="dominique59",
         database="discord"
     )
     cursor = db.cursor()
